ad967c768c8b906eb6f317ae5ff1bfda8a8bd82c
Ivairjr <ivair.junior@faulkner.edu>
2020-12-04 16:59:33 -0500

    Saving and Loading photos from/to Filesystem

--- src/hooks/usePhotoGallery.tsx
+++ src/hooks/usePhotoGallery.tsx
@@ -5,11 +5,12 @@ import { useStorage } from '@ionic/react-hooks/storage';
 import { isPlatform } from '@ionic/react';
 import { CameraResultType, CameraSource, CameraPhoto, Capacitor, FilesystemDirectory } from "@capacitor/core";
 
-
+const PHOTO_STORAGE = "photos";
 
 export function usePhotoGallery() {
 
     const { getPhoto } = useCamera();
+    const { deleteFile, getUri, readFile, writeFile } = useFilesystem();
 
     //Array to store captured photos
     const [photos, setPhotos] = useState<Photo[]>([]);
@@ -20,12 +21,33 @@ export function usePhotoGallery() {
         source: CameraSource.Camera,
         quality: 100
       });
+
+
       const fileName = new Date().getTime() + '.jpeg';
+        const savedFileImage = await savePicture(cameraPhoto, fileName);
         const newPhotos = [{
         filepath: fileName,
         webviewPath: cameraPhoto.webPath
         }, ...photos];
         setPhotos(newPhotos)
+    };
+
+    const savePicture = async (photo: CameraPhoto, fileName: string): Promise<Photo> => {
+      const base64Data = await base64FromPath(photo.webPath!);
+      const savedFile = await writeFile({
+        path: fileName,
+        data: base64Data,
+        directory: FilesystemDirectory.Data
+      });
+
+      const { get, set } = useStorage();
+    
+      // Use webPath to display the new image instead of base64 since it's
+      // already loaded into memory
+      return {
+        filepath: fileName,
+        webviewPath: photo.webPath
+      };
     };
   
     return {
