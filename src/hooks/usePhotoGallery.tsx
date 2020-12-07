3029c5a76ee4f7b3023d31295ccd606d5cbc489e
Ivairjr <ivair.junior@faulkner.edu>
2020-12-07 16:30:32 -0500

    Loading feature added

--- src/hooks/usePhotoGallery.tsx
+++ src/hooks/usePhotoGallery.tsx
@@ -11,9 +11,26 @@ export function usePhotoGallery() {
 
     const { getPhoto } = useCamera();
     const { deleteFile, getUri, readFile, writeFile } = useFilesystem();
+    const { get, set } = useStorage();
 
     //Array to store captured photos
     const [photos, setPhotos] = useState<Photo[]>([]);
+
+    useEffect(() => {
+      const loadSaved = async () => {
+        const photosString = await get(PHOTO_STORAGE);
+        const photos = (photosString ? JSON.parse(photosString) : []) as Photo[];
+        for (let photo of photos) {
+          const file = await readFile({
+            path: photo.filepath,
+            directory: FilesystemDirectory.Data
+          });
+          photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
+        }
+        setPhotos(photos);
+      };
+      loadSaved();
+    }, [get, readFile]);
   
     const takePhoto = async () => {
       const cameraPhoto = await getPhoto({
@@ -21,7 +38,7 @@ export function usePhotoGallery() {
         source: CameraSource.Camera,
         quality: 100
       });
-
+      
 
       const fileName = new Date().getTime() + '.jpeg';
         const savedFileImage = await savePicture(cameraPhoto, fileName);
@@ -30,7 +47,11 @@ export function usePhotoGallery() {
         webviewPath: cameraPhoto.webPath
         }, ...photos];
         setPhotos(newPhotos)
+        
+        set(PHOTO_STORAGE, JSON.stringify(newPhotos));
     };
+
+    
 
     const savePicture = async (photo: CameraPhoto, fileName: string): Promise<Photo> => {
       const base64Data = await base64FromPath(photo.webPath!);
@@ -40,7 +61,7 @@ export function usePhotoGallery() {
         directory: FilesystemDirectory.Data
       });
 
-      const { get, set } = useStorage();
+    
     
       // Use webPath to display the new image instead of base64 since it's
       // already loaded into memory
